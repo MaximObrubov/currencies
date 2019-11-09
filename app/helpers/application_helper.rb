500f7246b3b57ee046d3740844ef82c06bc0c467
Maxim Obrubov <m.obrubov@yandex.ru>
2019-11-10 02:02:23 +0400

    index page cosmetic

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  
+  def nav_li(text, url, active=false)
+    content_tag :li, class: "nav-item #{'active' if active}" do
+      link_to text, url, class: "nav-link #{"active" if active_path?(url)}"
+    end
+  end
+  
+  def active_path?(path)
+    request.path == path
+  end
+  
 end
