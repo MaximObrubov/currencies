9216e731907de1b832e6930cb6599c2b587aa6dc
Maxim Obrubov <m.obrubov@yandex.ru>
2019-11-09 13:25:01 +0400

    show current usd course on index#index page

--- app/components/cbr_api.rb
+++ app/components/cbr_api.rb
@@ -1,8 +1,30 @@
 module CBR_API
+  
+  require 'net/http'
+  
+  # returns parsed CBR currency cources relatively to RUB
+
   def self.courses
-    @courses ||= request()
+    raise Error.new("No API url") unless CBR_API_URL
+    request(url: CBR_API_URL)
   end
   
-  def self.request
+  # makes get request via Net::HTTP
+  # Params:
+  # +url+::  `:string` address to be requested
+  def self.request url:
+    uri = URI(url)
+    req = Net::HTTP::Get.new(uri, 'Content-Type' => 'application/json')
+    res = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => (uri.scheme == 'https')) do |http|
+      http.request(req)
+    end
+    
+    unless res.is_a?(Net::HTTPSuccess)
+      raise Error.new("`#{uri.host}` has returned bad status")
+    end
+    
+    JSON.parse(res.body, symbolize_names: true)
   end
+  
+  class Error < StandardError; end
 end
--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,5 +1,6 @@
 class IndexController < ApplicationController
   def index
+    @current_usd = CBR_API::courses[:Valute][:USD]
   end
   
   def admin
