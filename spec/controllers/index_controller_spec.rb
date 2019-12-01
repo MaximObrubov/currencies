1238d0863b57dce9b59279fccebba8e871d92b3d
Maxim Obrubov <m.obrubov@yandex.ru>
2019-12-01 23:46:51 +0400

    controller tests and some cosmetic

--- spec/controllers/index_controller_spec.rb
+++ spec/controllers/index_controller_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe IndexController, type: :controller do
-
+  describe "GET index" do
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template(:index)
+    end
+    
+    it "renders the index template" do
+      get :admin
+      expect(response).to render_template(:admin)
+    end
+  end
 end
